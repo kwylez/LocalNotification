160aa8085389dda54913420abc0c0f12e9e30812
Cory D. Wiles <kwylez@gmail.com>
2011-05-04 20:11:00 -0400

    removed unused methods and fixed memory leak.

--- Classes/LocalNotificationViewController.m
+++ Classes/LocalNotificationViewController.m
@@ -10,31 +10,19 @@
 
 @implementation LocalNotificationViewController
 
-
-
-/*
-// The designated initializer. Override to perform setup that is required before the view is loaded.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
 /*
 // Implement loadView to create a view hierarchy programmatically, without using a nib.
 - (void)loadView {
 }
 */
 
-
-
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
   
   UIApplication *app                = [UIApplication sharedApplication];
   UILocalNotification *notification = [[UILocalNotification alloc] init];
+  
+  if (notification == nil)
+    return;
   
   NSDate *notificationDate = [NSDate dateWithTimeIntervalSinceNow:10];
   
@@ -43,6 +31,8 @@
   notification.alertBody = @"Test Body";
   
   [app scheduleLocalNotification:notification];
+
+  [notification release];
   
   [super viewDidLoad];
 }
